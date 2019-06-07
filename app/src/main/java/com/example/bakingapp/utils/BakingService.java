65373120e032497c9623695bb457f3a5e4857d69
Mike Duong <mtanduong@gmail.com>
2019-06-06 21:06:00 -0700

    Implemented BakingService in utils package

---
+++ app/src/main/java/com/example/bakingapp/utils/BakingService.java
@@ -0,0 +1,15 @@
+package com.example.bakingapp.utils;
+
+import com.example.bakingapp.models.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface BakingService {
+
+    @GET(BakingApi.BAKING_PATH)
+    Call<List<Recipe>> getRecipes();
+    //Call<RecipeObject> getRecipes();
+}
