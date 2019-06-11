db0bdb307008e23c7b372de9ab7173ed842137d6
Mike Duong <mtanduong@gmail.com>
2019-06-10 21:57:20 -0700

    Implemented RecipeMasterFragment java class

--- app/src/main/java/com/example/bakingapp/fragments/RecipeMasterFragment.java
+++ app/src/main/java/com/example/bakingapp/fragments/RecipeMasterFragment.java
@@ -1,6 +1,129 @@
 package com.example.bakingapp.fragments;
 
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.example.bakingapp.R;
+import com.example.bakingapp.adapters.IngredientRecyclerViewAdapter;
+import com.example.bakingapp.adapters.StepRecyclerViewAdapter;
+import com.example.bakingapp.models.Ingredient;
+import com.example.bakingapp.models.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RecipeMasterFragment extends Fragment {
+
+    private static final String TAG = "MasterFragment";
+
+    @BindView(R.id.ingredient_recyclerview) RecyclerView ingredientRecyclerView;
+    @BindView(R.id.step_recyclerview) RecyclerView stepRecyclerView;
+    private List<Step> stepList;
+    private List<Ingredient> ingredientList;
+    private boolean isTablet;
+    private int ingredientViewPos;
+    private int stepViewPos;
+    private int index;
+    private int[] tracker;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.recipe_master_fragment, container, false);
+
+        ButterKnife.bind(this, view);
+
+        if(savedInstanceState == null) {
+
+            Bundle extra = getArguments();
+            ingredientList = extra.getParcelableArrayList("ingredients");
+            stepList = extra.getParcelableArrayList("steps");
+            isTablet = extra.getBoolean("tablet");
+            ingredientViewPos = extra.getInt("ingredientViewPos");
+            stepViewPos = extra.getInt("stepViewPos");
+            index = 0;
+        } else {
+
+            ingredientList = savedInstanceState.getParcelableArrayList("ingredients");
+            stepList = savedInstanceState.getParcelableArrayList("steps");
+            isTablet = savedInstanceState.getBoolean("tablet", false);
+            ingredientViewPos = savedInstanceState.getInt("ingredientViewPos");
+            stepViewPos = savedInstanceState.getInt("stepViewPos");
+            index = savedInstanceState.getInt("index");
+        }
+
+        tracker = new int[stepList.size()];
+
+        if(isTablet) {
+            tracker[index] = 1;
+        }
+
+        IngredientRecyclerViewAdapter ingredientAdapter = new IngredientRecyclerViewAdapter(getActivity(), ingredientList);
+        ingredientRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        ingredientRecyclerView.setAdapter(ingredientAdapter);
+
+        if(ingredientViewPos != 0) {
+            ingredientRecyclerView.getLayoutManager().scrollToPosition(ingredientViewPos);
+        }
+
+        StepRecyclerViewAdapter stepAdapter = new StepRecyclerViewAdapter(getActivity(), stepList, tracker);
+        stepRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        stepRecyclerView.setAdapter(stepAdapter);
+
+        if(stepViewPos != 0) {
+            stepRecyclerView.getLayoutManager().scrollToPosition(stepViewPos);
+        }
+
+        //TODO - Item Touch Listener
+        stepRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+        if(isTablet) {
+            //updateView(index);
+            //listener.setStep(index, stepList);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelableArrayList("ingredients", (ArrayList<? extends Parcelable>) ingredientList);
+        outState.putParcelableArrayList("steps", (ArrayList<? extends Parcelable>) stepList);
+        outState.putBoolean("tablet", isTablet);
+        outState.putInt("ingredientViewPos", ((LinearLayoutManager)ingredientRecyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition());
+        outState.putInt("stepViewPos", ((LinearLayoutManager)stepRecyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition());
+        outState.putInt("index", index);
+    }
 }
